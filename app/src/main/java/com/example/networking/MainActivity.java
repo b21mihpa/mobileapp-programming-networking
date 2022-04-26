fd5fb1aad46f2c40d06ef5bff68689baca4a7ef7
b21mihpa <b21mihpa@student.his.se>
2022-04-26 10:57:46 +0200

    added: new JsonTask instance

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
 
     private ArrayList<Mountain> mountains;
@@ -21,12 +21,12 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
-        Log.d("MainActivity", json);
+        Log.d("JSON_RESPONSE", json);
     }
 
 }
--- app/src/main/java/com/example/networking/MountainAdapter.java
+++ app/src/main/java/com/example/networking/MountainAdapter.java
@@ -1,3 +1,5 @@
+package com.example.networking;
+
 import android.view.View;
 import android.view.ViewGroup;
 
