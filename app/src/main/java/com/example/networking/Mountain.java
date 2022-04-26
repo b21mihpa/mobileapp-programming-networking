4f387ea70b36ddd7c79801779f26a95e8cfed947
b21mihpa <b21mihpa@student.his.se>
2022-04-27 01:07:00 +0200

    added: annotations to rename certain attributes during deserialization

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,13 +1,18 @@
 package com.example.networking;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Mountain {
-    private String ID;
+    @SerializedName("ID")
+    private String id;
     private String name;
     private String type;
     private String company;
     private String location;
     private String category;
-    private int size;
-    private int cost;
+    @SerializedName("size")
+    private int meters;
+    @SerializedName("cost")
+    private int feet;
     private Auxdata auxdata;
 }
